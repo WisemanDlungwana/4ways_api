fb4ac00d913159d85966cf6c73377fb8ac910d7b
Wiseman Dlungwana <siyethemba95@gmail.com>
2023-11-09 12:43:46 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,13 @@
 <?php
-$con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "techwise.mysql.database.azure.com", "wiseman", "Christforme#19", "4waysdb", 3306, MYSQLI_CLIENT_SSL);
+$db_host = "techwise.mysql.database.azure.com";
+$db_user = "wiseman";
+$db_pass = "Christforme#19";
+$db_name = "4waysdb";
 
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$con = mysqli_init();
+mysqli_ssl_set($con, NULL, NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($con, $db_host, $db_user, $db_pass, $db_name, 3306, MYSQLI_CLIENT_SSL);
+
 if (mysqli_connect_errno()) {
     $response = array('error' => true, 'message' => 'Database connection failed');
 }
@@ -12,7 +16,7 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-$result = $mysqli->query($sql);
+$result = $con->query($sql);
 
 if ($result->num_rows > 0) {
     // Login successful
@@ -32,5 +36,5 @@ if ($result->num_rows > 0) {
 header('Content-Type: application/json');
 echo json_encode($user);
 
-$mysqli->close();
+$con->close();
 ?>
